c1480b3b6d22348e7ec409c6c3a7b78f2f256548
Steven R. Loomis <srl295@gmail.com>
2021-09-06 11:02:46 -0500

    test - 2minute timer

--- src/main.cpp
+++ src/main.cpp
@@ -12,39 +12,67 @@
 
 Adafruit_7segment matrix = Adafruit_7segment();
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup()
+{
   matrix.begin(0x70);
   matrix.setBrightness(5);
 }
 
 uint32_t n = 0;
 
-void loop() {
-  for (uint16_t counter = 0x0; counter <= 0X50; counter++) {
+void printTime2(int m, int s)
+{
+  matrix.clear();
+  if (m > 0)
+  {
+    int m1 = m % 10;
+    int m0 = (m - m1) / 10;
+    if (m0)
+    {
+      matrix.writeDigitNum(0, m0, false);
+    }
+    matrix.writeDigitNum(1, m1, false);
+  }
+  // digit '2'
+  matrix.drawColon(s % 2 == 0);
+  int s1 = s % 10;
+  int s0 = (s - s1) / 10;
+  matrix.writeDigitNum(3, s0, false);
+  matrix.writeDigitNum(4, s1, false);
+}
+
+void printTime(int ss)
+{
+  int s = ss % 60;
+  int m = (ss - s) / 60;
+  printTime2(m, s);
+}
+
+void loop()
+{
+  int countdown = 2 * 60; // 2 minutes
+  for (int16_t counter = countdown; counter >= 0; counter--)
+  {
+    printTime(counter);
+    matrix.writeDisplay();
+    delay(1000);
+  }
+
+  // Done.
+  matrix.setBrightness(0);
+  int p = 0;
+  int j = 0;
+  for (;;)
+  {
+    j++;
     matrix.clear();
-    matrix.printNumber(counter, HEX);
-    if (counter >= 0x20 && counter < 0x40) {
-      matrix.writeDigitRaw(0, SPINNER(spinner_circ, (n--)));
-      matrix.writeDisplay();
-      delay(66);
-      matrix.writeDigitRaw(0, SPINNER(spinner_circ, (n--)));
-      matrix.writeDisplay();
-      delay(66);
-      matrix.writeDigitRaw(0, SPINNER(spinner_circ, (n--)));
-      matrix.writeDisplay();
-      delay(66);
-    } else if (counter >= 0x40 && counter <= 0x50) {
-      matrix.writeDigitRaw(0, SPINNER(spinner_rrxing, (n++)));
-      matrix.writeDisplay();
-      delay(200);
-    } else {
-      matrix.writeDigitRaw(0, SPINNER(spinner_circ, (n++)));
-      matrix.writeDisplay();
-      delay(100);
-      matrix.writeDigitRaw(0, SPINNER(spinner_circ, (n++)));
-      matrix.writeDisplay();
-      delay(100);
+    matrix.writeDigitRaw(p, SPINNER(spinner_circ, (n++)));
+    matrix.writeDisplay();
+    delay(66);
+    if ((j % 127) == 0) {
+      p++;
+      if(p == 2) p++;
+      if(p == 5) p = 0;
     }
   }
 }
